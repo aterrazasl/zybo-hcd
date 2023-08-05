16b2db0d29ee75dadae1df256491c5525a08d22a
Antonio <antonioterrazaslara@hotmail.com>
2023-08-04 23:05:33 -0500

    Added definitions for bRequest

--- usb-test/src/hcd/hid.h
+++ usb-test/src/hcd/hid.h
@@ -8,8 +8,16 @@ extern "C" {
 
 #include "xil_types.h"
 #include "xil_cache.h"
+#include "hcd.h"
 
 
+#define GET_REPORT 0x01
+#define GET_IDLE 0x02
+#define GET_PROTOCOL 0x03
+#define Reserved 0x04
+#define SET_REPORT 0x09
+#define SET_IDLE 0x0A
+#define SET_PROTOCOL 0x0B
 
 
 void hid_callbackHandler(void *CallBackRef, u32 Mask);
